43ff2f7c08e6c9844811a755d17c65dba9d9c9b3
brightfuture-8888 <ssato@smith.edu>
2024-04-22 19:34:17 -0400

    A

--- CSC120 A3/Conversation.java
+++ CSC120 A3/Conversation.java
@@ -6,66 +6,66 @@ class Conversation {
   /***
    * This method asks you to put numbers of how many rounds you want to play.
    * It takes the user to put conversations and makes it to a basic chatbot.
-   * It will give the user mirrorable words.      
+   * It will give the user mirrorable words.
+   * 
    * @param args
    */
-  public static void main(String[] args) { 
+  public static void main(String[] args) {
     int count = 0;
-    String normalResponse[] = {"Yeah", "Right", "True"};
+    String normalResponse[] = { "Yeah", "Right", "True" };
 
-    Scanner sc = new Scanner (System.in); // build scanner to read the test 
-    String userinput= ""; 
+    Scanner sc = new Scanner(System.in); // build scanner to read the test
+    String userinput = "";
 
-    System.out.println("How many rounds?"); 
-    int roundnumber;  //user input here - will get overwritten 
-    roundnumber = sc.nextInt(); //store number from user input
-    sc.nextLine();  // clear out the new line
+    System.out.println("How many rounds?");
+    int roundnumber; // user input here - will get overwritten
+    roundnumber = sc.nextInt(); // store number from user input
+    sc.nextLine(); // clear out the new line
 
-    System.out.println("Hi there!  What's on your mind?");  
-     
+    System.out.println("Hi there!  What's on your mind?");
 
-    for (int i =0; i<roundnumber; i++){ //create a loop that repeats for that amount of times   
+    for (int i = 0; i < roundnumber; i++) { // create a loop that repeats for that amount of times
       userinput = sc.nextLine();
 
-      String[] words = userinput.split(" ");  
-      //System.out.println(Arrays.toString(words)); 
-      boolean canned = true; 
+      String[] words = userinput.split(" ");
+      // System.out.println(Arrays.toString(words));
+      boolean canned = true;
 
-      for (int j = 0; j < words.length; j++){
-        
-        String word = words[j]; 
-        
-        if (word.equals("I")){
+      for (int j = 0; j < words.length; j++) {
+
+        String word = words[j];
+
+        if (word.equals("I")) {
           words[j] = "you";
-          canned = false; 
-        } else if (word.equals("you")){
+          canned = false;
+        } else if (word.equals("you")) {
           words[j] = "I";
-          canned = false; 
-        } else if (word.equals("me")){
+          canned = false;
+        } else if (word.equals("me")) {
           words[j] = "you";
-          canned = false; 
-        } else if (word.equals("am")){
+          canned = false;
+        } else if (word.equals("am")) {
           words[j] = "are";
-          canned = false; 
-        } else if (word.equals("my")){
+          canned = false;
+        } else if (word.equals("my")) {
           words[j] = "your";
-          canned = false; 
-        } else if (word.equals("your")){
+          canned = false;
+        } else if (word.equals("your")) {
           words[j] = "my";
-          canned = false; 
+          canned = false;
         }
       }
-      
-      if (canned == true){
+
+      if (canned == true) {
 
         System.out.println(normalResponse[count]);
-        count+=1;
-        if (normalResponse.length == (count)){
+        count += 1;
+        if (normalResponse.length == (count)) {
           count = 0;
         }
 
       } else {
-        for (int n = 0; n < words.length; n++){
+        for (int n = 0; n < words.length; n++) {
           System.out.print(words[n] + " ");
         }
         System.out.println("");
@@ -74,4 +74,3 @@ class Conversation {
     sc.close();
   }
 }
-
